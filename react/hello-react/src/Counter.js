6b1406ae3010b11b786f9b9e6e1131e6bd13c5fd
hik3562 <36418489+lllilllilllilili@users.noreply.github.com>
2021-07-08 08:21:51 +0900

    feat: 🎸 mission1
    75
    
    ✅ Closes: 75

---
+++ react/hello-react/src/Counter.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  );
+};
+
+export default Counter;
