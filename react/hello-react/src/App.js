06f9dbcd44b4a942dd92fdd3cc9ac51e217456ca
hik3562 <36418489+lllilllilllilili@users.noreply.github.com>
2021-07-09 14:03:15 +0900

    feat: 🎸 input tag 생성

--- react/hello-react/src/App.js
+++ react/hello-react/src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Fragment } from "react";
 
 function App() {
   return (
---
+++ react/hello-react/src/EventHandler.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const EventHandler = () => {
+  return (
+    <div>
+      <input></input>
+      <input></input>
+    </div>
+  );
+};
