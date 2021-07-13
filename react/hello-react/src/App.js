aaa9d569812dd07444d2dc6b1015b69da4f731dc
hik3562 <36418489+lllilllilllilili@users.noreply.github.com>
2021-07-13 19:29:54 +0900

    feat: 🎸 입력창에 작성한 숫자를 추가버튼 클릭시 화면에 렌더링

--- react/hello-react/src/App.js
+++ react/hello-react/src/App.js
@@ -1,15 +1,24 @@
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const array = [1, 2, 4, 8];
-  const arrayItems = array.map((number) => <ul>{number}</ul>);
-  onkeypress = (event) => {
-    console.log("event");
+  const [number, setNumber] = useState([1, 2, 3, 4]);
+  const inputValue = useRef();
+  let arrayItems = number.map((number) => (
+    <li key={number.toString()}>{number}</li>
+  ));
+  const onClick = (event) => {
+    const newArrayItem = [...number, inputValue.current.value];
+    console.log(newArrayItem);
+    setNumber(newArrayItem);
+    arrayItems = number.map((number) => (
+      <li key={number.toString()}>{number}</li>
+    ));
   };
   return (
     <div className="App">
-      <input></input>
-      <button onClick={onkeypress}>추가</button>
+      <input type="text" name="inputValue" ref={inputValue}></input>
+      <button onClick={onClick}>추가</button>
       {arrayItems}
     </div>
   );
